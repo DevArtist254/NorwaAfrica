21a8c3c4a8bec73e6601d89b303f02a335fab0f4
DevArtist <53369947+DevArtist254@users.noreply.github.com>
2021-03-25 11:39:47 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,12 +1,3 @@
-const tl = gsap.timeline({defaults: {ease: "power1.out"}})
-
-tl.to(".text", {y: "0%", duration: 1, stagger: 0.25})
-tl.to(".slider", {y: "-100%", duration: 1.5, delay: 0.5})
-tl.to(".intro", {y: "-100%", duration: 1}, "-=1")
-tl.fromTo("nav", {opacity: 0}, {opacity: 1, duration: 1})
-tl.fromTo(".hero-sect__heading", {opacity: 0}, {opacity: 1, duration: 1}, "-=1")
-
-
 const btnDropdown = document.querySelectorAll(".btn-dropDown")
 
 //1. select of all button and loop thru each
@@ -46,4 +37,73 @@ function closeContent() {
       btn.style.transform = "rotate(0deg)"
     }
   }
+}
+
+const slides = document.querySelectorAll(".move");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+const auto = true;
+const intervalTime = 5000;
+let slideInterval;
+
+const nextSilde = () => {
+  const current = document.querySelector('.current')
+  //remove current class
+  current.classList.remove('current');
+
+  //Check for next slide
+  if(current.nextElementSibling){
+    //add current to next sibling
+    current.nextElementSibling.classList.add('current')
+  }else {
+    slides[0].classList.add('current');
+  }
+
+  setTimeout(() => {
+    current.classList.remove('current')
+  })
+}
+
+const prevSilde = () => {
+  const current = document.querySelector('.current')
+  //remove current class
+  current.classList.remove('current');
+
+  //Check for next slide
+  if(current.previousElementSibling){
+    //add current to next sibling
+    current.previousElementSibling.classList.add('current')
+  }else {
+    //add current to class
+    slides[slides.length - 1].classList.add('current');
+  }
+
+  setTimeout(() => {
+    current.classList.remove('current')
+  })
+}
+
+
+next.addEventListener('click', e => {
+  nextSilde()
+
+  if(auto) {
+    clearInterval(slideInterval)
+    slideInterval = setInterval(nextSilde, intervalTime)
+  }
+})
+
+prev.addEventListener('click', e => {
+  prevSilde()
+
+  if(auto) {
+    clearInterval(slideInterval)
+    slideInterval = setInterval(nextSilde, intervalTime)
+  }
+})
+
+
+if(auto) {
+  //Run next slide at interval time
+  slideInterval = setInterval(nextSilde, intervalTime)
 }
